475f67e6d426996681c2620834725b5983c4cc97
Daniel Mittereder <danielmittereder@gmail.com>
2024-02-17 16:53:09 +0100

    Screw multiple commands. KISS

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -5,10 +5,15 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	const storageKey = 'tab.closing.regex';
 
-	async function closeTabsViaRegex(regexString?: string) {
-		regexString ??= await vscode.window.showInputBox({
+	// The command has been defined in the package.json file
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('close-tabs-via-regex.close', async (arg) => {
+		if (arg !== undefined && arg !== null && typeof arg !== 'string') {
+			throw new Error('Expected `args` of `close-tabs-via-regex.close` to be a string.');
+		}
+
+		const regexString = arg ?? await vscode.window.showInputBox({
 			placeHolder: 'RegExp e.g. spec\\.ts$',
 			validateInput(value) {
 				try {
@@ -28,29 +33,9 @@ export function activate(context: vscode.ExtensionContext) {
 		const allTabs = vscode.window.tabGroups.all.flatMap(tabGroup => tabGroup.tabs);
 		const tabsToClose = allTabs.filter(tab => regex.test(tab.label));
 		await vscode.window.tabGroups.close(tabsToClose);
-
-		await context.globalState.update(storageKey, regexString);
-	}
-
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable1 = vscode.commands.registerCommand('close-tabs-via-regex.close', async (arg) => {
-		if (arg !== undefined && arg !== null && typeof arg !== 'string') {
-			throw new Error('Expected `args` of `close-tabs-via-regex.close` to be a string.');
-		}
-
-		const regexString = arg ?? context.globalState.get(storageKey);
-		await closeTabsViaRegex(regexString);
 	});
 
-	let disposable2 = vscode.commands.registerCommand('close-tabs-via-regex.prompt-close', async () => {
-		await closeTabsViaRegex();
-	});
-
-	context.subscriptions.push(disposable1);
-	context.subscriptions.push(disposable2);
+	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
