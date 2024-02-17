1fffcbf7e1050f9764b6ca42b31d8f4ac9b1bffc
Daniel Mittereder <danielmittereder@gmail.com>
2024-02-17 01:26:45 +0100

    Add close-tabs-via-regex command

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -8,16 +8,32 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "close-tabs-via-regex" is now active in the web extension host!');
+	// console.log('Congratulations, your extension "close-tabs-via-regex" is now active in the web extension host!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('close-tabs-via-regex.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('close-tabs-via-regex.close-tabs-via-regex', async (regexString) => {
 		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Close Tabs via Regex in a web extension host!');
+		regexString ??= await vscode.window.showInputBox({
+			placeHolder: 'RegExp e.g. spec\\.ts$',
+			validateInput(value) {
+				try {
+					new RegExp(value);
+				} catch (err) {
+					return `${err}`;
+				}
+			},
+		});
+
+		if (!regexString) { return; }
+
+		const regex = new RegExp(regexString);
+
+		const allTabs = vscode.window.tabGroups.all.flatMap(tabGroup => tabGroup.tabs);
+		const tabsToClose = allTabs.filter(tab => regex.test(tab.label));
+		await vscode.window.tabGroups.close(tabsToClose);
 	});
 
 	context.subscriptions.push(disposable);
